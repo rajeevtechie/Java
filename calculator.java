c13947f52d44ee7ced57f885d0a2cad811992138
rajeevtechie <rajeev.techie380@gmail.com>
2026-01-12 10:37:45 +0530

    created a calculator using java

---
+++ calculator.java
@@ -0,0 +1,25 @@
+public class calculator {
+   static int sum(int a,int b){
+        return a+b;
+    }
+
+    static int difference(int a, int b){
+        return a-b;
+    }
+
+    static int product(int a, int b){
+        return a*b;
+    }
+
+    static int division(int a, int b){
+        return a/b;
+    }
+  public static void main(String[] args) {
+    
+    System.out.println(sum(2,5));
+    System.out.println(difference(2,5));
+    System.out.println(product(2,5));
+    System.out.println(division(2,5));
+  }
+}
+
