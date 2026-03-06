e78bcd5872ed5872bec970bb235f88348e6e34fa
rajeevtechie <rajeev.techie380@gmail.com>
2026-03-06 22:59:51 +0530

    practised few more exception concepts

--- practise/exception.java
+++ practise/exception.java
@@ -1,9 +1,18 @@
 public class exception {
     public static void main(String[] args) {
         int i=0;
-        int j=0;
+        int j=18;
+        int arr[] = new int[5];
         try{
             j = j/i;
+            System.err.println(arr[1]);
+            System.err.println(arr[6]);
+        }
+        catch(ArithmeticException e){
+            System.out.println("can't divide by zero.");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Accessing out of index element.");
         }
         catch(Exception e){
             System.out.println("something went wrong...");
